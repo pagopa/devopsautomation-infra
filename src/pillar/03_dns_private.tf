af12dfb6d0cbe7d6cfa7fdefbf3621bc1dacfe4e
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2022-07-15 16:47:07 +0200

    Init_project (#1)
    * init states
    
    * github action updated
    
    * precommit fix
    
    * changed devopslab to devopsautomation
    
    * fix precommit warnings

--- src/pillar/03_dns_private.tf
+++ src/pillar/03_dns_private.tf
@@ -1,7 +1,7 @@
 #
-# internal.devopslab...
+# internal.devopsautomation...
 #
-resource "azurerm_private_dns_zone" "internal_devopslab" {
+resource "azurerm_private_dns_zone" "internal_devopsautomation" {
   count               = (var.prod_dns_zone_prefix == null || var.external_domain == null) ? 0 : 1
   name                = local.dns_zone_private_name
   resource_group_name = azurerm_resource_group.rg_vnet.name
@@ -9,19 +9,19 @@ resource "azurerm_private_dns_zone" "internal_devopslab" {
   tags = var.tags
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "internal_devopslab_vnet" {
+resource "azurerm_private_dns_zone_virtual_network_link" "internal_devopsautomation_vnet" {
   name                  = local.vnet_resource_group_name
   resource_group_name   = azurerm_resource_group.rg_vnet.name
-  private_dns_zone_name = azurerm_private_dns_zone.internal_devopslab[0].name
+  private_dns_zone_name = azurerm_private_dns_zone.internal_devopsautomation[0].name
   virtual_network_id    = module.vnet.id
 
   tags = var.tags
 }
 
 #
-# internal.lab.devopslab...
+# internal.lab.devopsautomation...
 #
-resource "azurerm_private_dns_zone" "internal_lab_devopslab" {
+resource "azurerm_private_dns_zone" "internal_lab_devopsautomation" {
   count               = (var.lab_dns_zone_prefix == null || var.external_domain == null) ? 0 : 1
   name                = local.dns_zone_lab_private_name
   resource_group_name = azurerm_resource_group.rg_vnet.name
@@ -29,10 +29,10 @@ resource "azurerm_private_dns_zone" "internal_lab_devopslab" {
   tags = var.tags
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "internal_lab_devopslab_vnet" {
+resource "azurerm_private_dns_zone_virtual_network_link" "internal_lab_devopsautomation_vnet" {
   name                  = local.vnet_resource_group_name
   resource_group_name   = azurerm_resource_group.rg_vnet.name
-  private_dns_zone_name = azurerm_private_dns_zone.internal_lab_devopslab[0].name
+  private_dns_zone_name = azurerm_private_dns_zone.internal_lab_devopsautomation[0].name
   virtual_network_id    = module.vnet.id
 
   tags = var.tags
